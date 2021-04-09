995fc605aac14f0cf2d8c68a7a7c87824970c57f
gregH <contact@greghenry.xyz>
2021-04-09 10:18:17 +0200

    ✨ Added constant module

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.4] - 2021-04-09
+
++ added module constants
+
 ## [0.3.3] - 2021-04-09
 
 + added constants
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,11 +3,14 @@ pub mod geometry;
 /// Log aliases.
 #[macro_use]
 pub mod log;
+/// Constants.
+pub mod constants;
 /// Collection of functions for matrix usage.
 pub mod matrix;
 /// Numerical algorithms toolbox.
 pub mod numerical_algorithms;
 
+pub use self::constants::*;
 pub use self::geometry::*;
 pub use self::log::*;
 pub use self::matrix::*;
